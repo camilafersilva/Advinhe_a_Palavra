8ea922139aeb50ad26359d5f8fed81b723ce4fac
camilafersilva <camilafsufmg@gmail.com>
2023-12-26 21:32:09 -0300

    Novas Categorias

--- src/data/word.js
+++ src/data/word.js
@@ -5,4 +5,14 @@ export const wordsList = {
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
     programação: ["Linguagem", "Framework", "JavaScript", "React"],
     alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Atletismo"],
+instrumento : ["Violino", "Guitarra", "Piano", "Flauta", "Bateria"],
+filme: ["Ação", "Comédia", "Drama", "Ficção Científica", "Animação"],
+país: ["Brasil", "Estados Unidos", "França", "Japão", "Austrália"],
+bebida: ["Café", "Chá", "Suco", "Refrigerante", "Água"],
+cor: ["Azul", "Verde", "Vermelho", "Amarelo", "Roxo"],
+animal: ["Leão", "Elefante", "Girafa", "Cachorro", "Gato"],
+profissão: ["Médico", "Professor", "Engenheiro", "Advogado", "Artista"],
+cidade: ["Nova York", "Paris", "Tóquio", "Rio de Janeiro", "Londres"],
+hobby: ["Leitura", "Pintura", "Ciclismo", "Cozinhar", "Viajar"]
   };
